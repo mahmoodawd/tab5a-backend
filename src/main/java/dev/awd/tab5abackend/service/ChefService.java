0e0fc7f9d6f8df470d35d07f403ad9ec2fd9d41f
Ma7moud3wd <mahmooodawd@gmail.com>
2026-02-21 15:29:26 +0200

    Chefs: Introduce logs

--- src/main/java/dev/awd/tab5abackend/service/ChefService.java
+++ src/main/java/dev/awd/tab5abackend/service/ChefService.java
@@ -1,6 +1,5 @@
 package dev.awd.tab5abackend.service;
 
-import dev.awd.tab5abackend.dto.request.CategoryRequestDto;
 import dev.awd.tab5abackend.dto.request.ChefRequestDto;
 import dev.awd.tab5abackend.dto.response.ChefResponseDto;
 import dev.awd.tab5abackend.exception.ChefAlreadyExistException;
--- src/main/java/dev/awd/tab5abackend/service/ChefServiceImpl.java
+++ src/main/java/dev/awd/tab5abackend/service/ChefServiceImpl.java
@@ -9,12 +9,14 @@ import dev.awd.tab5abackend.mapper.ChefMapper;
 import dev.awd.tab5abackend.model.Chef;
 import dev.awd.tab5abackend.repository.ChefRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@RequiredArgsConstructor
 @Service
+@RequiredArgsConstructor
+@Slf4j
 public class ChefServiceImpl implements ChefService {
 
     private final ChefRepository chefRepository;
@@ -24,28 +26,53 @@ public class ChefServiceImpl implements ChefService {
 
     @Override
     public List<ChefResponseDto> findAll() {
-        return chefRepository
+        log.debug("Fetching all chefs");
+
+        List<ChefResponseDto> chefs = chefRepository
                 .findAll()
                 .stream()
                 .map(chefMapper::chefToChefResponseDto)
                 .toList();
+
+        log.info("Retrieved {} chefs", chefs.size());
+        return chefs;
     }
 
     @Override
     public ChefResponseDto findById(Long id) {
+        log.debug("Fetching chef with id: {}", id);
+
         return chefRepository.findById(id)
                 .map(chefMapper::chefToChefResponseDto)
-                .orElseThrow(() -> new ChefNotFoundException(id));
+                .orElseThrow(() -> {
+                    log.warn("chef not found with id: {}", id);
+                    return new ChefNotFoundException(id);
+                });
     }
 
     @Override
     public ChefResponseDto save(ChefRequestDto chefRequest) throws ChefAlreadyExistException {
-        if (chefRepository.existsByName(chefRequest.getName()))
+        log.info("Registering New Chef: '{}'", chefRequest.getName());
+
+        if (chefRepository.existsByName(chefRequest.getName())) {
+            log.warn("Chef registration failed - duplicate name: '{}' ", chefRequest.getName());
             throw new ChefAlreadyExistException(chefRequest.getName());
+        }
+
+        log.debug("Mapping chef DTO to entity");
         Chef chefToSave = chefMapper.chefRequestDtoToChef(chefRequest);
+
+        log.debug("Uploading chef image");
         String imageStoragePath = uploadService.uploadImage(chefRequest.getAvatar(), ImageType.CHEF);
         chefToSave.setAvatar(imageStoragePath);
+
+        log.debug("Saving chef to DB");
         Chef savedChef = chefRepository.save(chefToSave);
+
+        log.info("Chef created successfully: id={}, name='{}', avatarPath={}",
+                savedChef.getId(), savedChef.getName(), imageStoragePath);
+
+        log.debug("Mapping chef entity to response DTO");
         return chefMapper.chefToChefResponseDto(savedChef);
     }
 }
